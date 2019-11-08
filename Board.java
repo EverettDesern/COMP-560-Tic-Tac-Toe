c44d62f99ff623487178de430ff9429272098437
Everett Desern <edesern@live.unc.edu>
2019-11-08 15:03:27 -0500

    added stuff to board class

--- Board.java
+++ Board.java
@@ -17,6 +17,67 @@ public class Board {
         }
     }
 
+    public void newGame() {
+        for(int i = 0; i < this.size; i++) {
+            for(int j = 0; j < this.size; j++) {
+                for(int p = 0; p < this.size; p++) {
+                    this.gameBoard[i][j][p] = new Tile();
+                }
+            }
+        }
+        this.won = false;
+        this.full = false;
+    }
+
+    public void printBoard() {
+
+    }
+
+    public boolean isGameFull() {
+        // checks if all moves have been made.
+        for(int i = 0; i < this.size; i++) {
+            for(int j = 0; j < this.size; j++) {
+                for(int p = 0; p < this.size; p++) {
+                    if(this.gameBoard[i][j][p].getValue() == " ") {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isGameWon() {
+        // checks if a row, column, diagonal, and 3D space has the same value.. if so, the game is finished.
+        if(checkRow()) {
+            return true;
+        }
+        if(checkCol()) {
+            return true;
+        }
+        if(checkDiag()) {
+            return true;
+        }
+        if(check3D()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkRow() {
+        return false;
+    }
+
+    public boolean checkCol() {
+        return false;
+    }
+
+    public boolean checkDiag() {
+        return false;
+    }
+    public boolean check3D() {
+        return false;
+    }
+
 
 
 
