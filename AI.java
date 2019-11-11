34702112d8a957f786395d7783c6269ae35520a8
Everett Desern <edesern@live.unc.edu>
2019-11-11 01:02:45 -0500

    added stuff

---
+++ AI.java
@@ -0,0 +1,18 @@
+public class AI {
+
+    public AI() {
+        this.learningRate = 0.3;
+        this.exploration = 0.9;
+        this.decay = 0.01;
+        this.discount = 0.01;
+    }
+
+    public void reward() {
+
+        
+    }
+
+
+
+
+}
---
+++ TrainAI.java
@@ -0,0 +1,25 @@
+public class TrainAI {
+
+    public void train(int epochs, AI AIOne, AI AITwo) {
+        // AIOne is winningCounts[0] and AITwo is winningCounts[1]
+        int[] winningCounts = new int[2];
+        winningCounts[0] = 0;
+        winningCounts[1] = 0;
+
+        for(int i = 0; i < epochs; i++) {
+            
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        AI AIOne = new AI();
+        AI AITwo = new AI();
+
+        Scanner scan = new Scanner();
+    }
+
+
+
+}
